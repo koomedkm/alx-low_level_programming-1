c0c060c1991cab8beaa2be45d46639bfdf7792a7
wechu07 <wechuli017@gmail.com>
2021-06-18 10:37:38 -0700

    correct advaned file 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,52 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
+/* betty style doc for function main goes there */
 /**
- *  * main - prints all possible different combinations of three digits
- *   * Return: Always 0 (Success)
- *    */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int n, m, l;
+	int i;
+	int j;
+	int k;
 
-			for (n = 48; n < 58; n++)
-					{
-								for (m = 49; m < 58; m++)
-											{
-															for (l = 50; l < 58; l++)
-																			{
-																								if (l > m && m > n)
-																													{
-																																			putchar(n);
-																																								putchar(m);
-																																													putchar(l);
-																																																		if (n != 55 || m != 56)
-																																																								{
-																																																															putchar(',');
-																																																																					putchar(' ');
-																																																																										}
-																																																						}
-																											}
-																	}
-									}
-				putchar('\n');
-					return (0);
+	i = 48;
+	j = 49;
+	k = 50;
+	while  ((i < 56) && (j < 57) && (k < 58))
+	{
+		putchar(i);
+		putchar(j);
+		putchar(k);
+		if ((i == 55) && (j == 56) && (k == 57))
+		{
+			putchar('\n');
+			i++;
+			j++;
+			k++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (k < 57)
+			{
+				k++;
+			}
+			else if ((j < 56) && (k == 57))
+			{
+				k = 1 + ++j;
+			}
+			else
+			{
+				j = 1 + ++i;
+				k = 1 + j;
+			}
+		}
+	}
+	return (0);
 }
