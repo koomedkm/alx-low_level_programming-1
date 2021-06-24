3f8cd2b711949b29a9b77ffb38222fb58602ae4a
wechu07 <wechuli017@gmail.com>
2021-06-24 14:21:28 -0700

    correcting task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,13 +1,12 @@
 #include "holberton.h"
 
 /**
- *  * more_numbers - prints 10 times the numbers, from 0 to 14
- *   * followed by a new line
- *    */
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
 void more_numbers(void)
 {
 	int i, j;
-
 
 	for (i = 0; i < 10; i++)
 	{
