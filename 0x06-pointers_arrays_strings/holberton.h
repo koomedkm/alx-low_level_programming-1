edb65571cd30d8e69d6a7a7c525fe960e38cd800
wechu07 <wechuli017@gmail.com>
2021-07-01 10:17:36 -0700

    update header file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,3 +13,5 @@ char *rot13(char *);
 int _strlen(char *s);
 int _putchar(char c);
 void print_number(int n);
+
+#endif
