632a3e9064f0b84e321f5d15b79bd457186eb12e
wechu07 <wechuli017@gmail.com>
2021-07-01 10:27:51 -0700

    add advanced file 1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * *rot13 - encoding string
+ * @s: value to be checked
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+		return (s);
+}
