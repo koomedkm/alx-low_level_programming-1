9cef140524e1641c97e5beb3de9282c7829249a5
wechu07 <wechuli017@gmail.com>
2021-07-19 18:07:02 +0300

    correct file 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,14 +5,14 @@
 /**
  * main - prints the name of
  * the file it was compiled
- * from followed by a new 
+ * from followed by a new
  * line
  *
  * Return: 0
  */
 
-int main()
+int main(void)
 {
-	printf("%s\n", _FILE_ );
+	printf("%s\n", _FILE_);
 	return (0);
 }
